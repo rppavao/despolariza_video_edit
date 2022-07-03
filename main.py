540d1aeb7cf1b4dbea4a66f7851e63e1fcb39d85
rppavao <rafaelpppavao@gmail.com>
2022-07-03 01:21:25 +0100

    new_gui

--- main.py
+++ main.py
@@ -5,38 +5,103 @@ Created on Sat Jun  4 23:28:37 2022
 @author: Rafael Pavão
 """
 from video_edit_classes import *
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
+from screen import Ui_MainWindow
 
-path = input("File location: ")
-
-video_name1 = input("Name of first video (mp4): ")
-audio_name1 = input("Name of first audio (wav): ")
-
-video_name2 = input("Name of second video (mp4): ")
-audio_name2 = input("Name of second audio (wav): ")
-
-video_name3 = input("Name of third video (optional): ")
-
-print(">>>Uploading files...")
-
-video1 = video(video_name1,path)
-video1.upload_audio(audio_name1)
-video2 = video(video_name2,path)
-video2.upload_audio(audio_name2)
-if video_name3 != '':
-    video3 = video(video_name3,path)
-
-print(">>>Finished uploading!")
-
-cutoff = int( input("Specify sound cutoff (in dBFS [-inf,0]) for third video: ") )
-resolution = int( input("Time resolution (seconds): ") )
-volume_reduction = 0 #int( input("Reduction factor (in dBFS [-inf,0]): ") )
-
-edit_instance = edit_tools(video1,video2,video3=video3,path=path,parameters=(cutoff, \
-                                                resolution,volume_reduction))
+class videoEditGui(qtw.QMainWindow):
     
-print(">>>Video processing started...")
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.set_directory_tree()
+        self.video_list = []
+        self.audio_list = []
+        
+        self.ui.treeView.doubleClicked.connect(self.treeView_doubleClicked)
+        
+    def set_directory_tree(self):
+        model = qtw.QFileSystemModel()
+        model.setRootPath(qtc.QDir.currentPath())
+        
+        treeView = self.ui.treeView
+        treeView.setModel(model)
+        
+    def treeView_doubleClicked(self,index):
+        file_name = index.model().filePath(index)
+        
+        if file_name[-3:] == 'mp4':
+            if len(self.video_list) < 3:
+                self.video_list.append(file_name)
+            else:
+                self_video_list = [file_name]
 
-edit_instance.concat_video_by_sound()
+        if file_name[-3:] == 'wav':
+            if len(self.audio_list) < 3:
+                self.audio_list.append(file_name)
+            else:
+                self_audio_list = [file_name]
+                
+        videoLabel = {0:self.ui.labelVideo1, 1:self.ui.labelVideo2, 2:self.ui.labelVideo3}
+        audioLabel = {0:self.ui.labelAudio1, 1:self.ui.labelAudio2}
+        
+        for i in range(len(self.audio_list)):
+            try:
+                label = audioLabel[i]
+                name = self.audio_list[i].split('/')[-1]
+                label.setText(name)
+            except KeyError:
+                continue
+            
+        for i in range(len(self.video_list)):
+            try:
+                label = videoLabel[i]
+                name = self.video_list[i].split('/')[-1]
+                label.setText(name)
+            except KeyError:
+                continue
+        
+# path = input("File location: ")
 
-print(">>>Video processing ended!")
-print(">>>New file name 'final_video.mp4' was created!")
+# video_name1 = input("Name of first video (mp4): ")
+# audio_name1 = input("Name of first audio (wav): ")
+
+# video_name2 = input("Name of second video (mp4): ")
+# audio_name2 = input("Name of second audio (wav): ")
+
+# video_name3 = input("Name of third video (optional): ")
+
+# print(">>>Uploading files...")
+
+# video1 = video(video_name1,path)
+# video1.upload_audio(audio_name1)
+# video2 = video(video_name2,path)
+# video2.upload_audio(audio_name2)
+# if video_name3 != '':
+#     video3 = video(video_name3,path)
+
+# print(">>>Finished uploading!")
+
+# cutoff = int( input("Specify sound cutoff (in dBFS [-inf,0]) for third video: ") )
+# resolution = int( input("Time resolution (seconds): ") )
+# volume_reduction = 0 #int( input("Reduction factor (in dBFS [-inf,0]): ") )
+
+# edit_instance = edit_tools(video1,video2,video3=video3,path=path,parameters=(cutoff, \
+#                                                 resolution,volume_reduction))
+    
+# print(">>>Video processing started...")
+
+# edit_instance.concat_video_by_sound()
+
+# print(">>>Video processing ended!")
+# print(">>>New file name 'final_video.mp4' was created!")
+
+
+if __name__ == "__main__" :
+    app = qtw.QApplication([])
+    screen = videoEditGui()
+    screen.show()
+    app.exec_()
--- screen.py
+++ screen.py
@@ -1,29 +1,110 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 26 22:20:14 2022
 
-@author: Rafael Pavão
-"""
+# Form implementation generated from reading ui file 'main_screen.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
 
-import os
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class directory:
-    def __init__(self,in_directory=False):
-        if in_directory == False:
-            self.directory = os.getcwd().replace('\\','/')
-        else:
-            self.directory = in_directory
-        self.directory_list = self.directory.split('/')
-        self.subdirectory_list = []
-        self.files_list = []
-        
-    def get_subdir_and_files(self):
-        for obj in os.scandir(self.directory):
-            if obj.is_dir():
-                self.subdirectory_list.append(obj.path.replace('\\','/'))
-            else:
-                file = obj.path.split('\\')[-1]
-                self.files_list.append(file)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1034, 816)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.treeView = QtWidgets.QTreeView(self.centralwidget)
+        self.treeView.setGeometry(QtCore.QRect(20, 30, 771, 281))
+        self.treeView.setObjectName("treeView")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 10, 51, 16))
+        self.label.setObjectName("label")
+        self.labelVideo1 = QtWidgets.QLabel(self.centralwidget)
+        self.labelVideo1.setGeometry(QtCore.QRect(820, 40, 171, 16))
+        self.labelVideo1.setObjectName("labelVideo1")
+        self.labelAudio1 = QtWidgets.QLabel(self.centralwidget)
+        self.labelAudio1.setGeometry(QtCore.QRect(820, 70, 171, 16))
+        self.labelAudio1.setObjectName("labelAudio1")
+        self.labelVideo2 = QtWidgets.QLabel(self.centralwidget)
+        self.labelVideo2.setGeometry(QtCore.QRect(820, 100, 171, 16))
+        self.labelVideo2.setObjectName("labelVideo2")
+        self.labelAudio2 = QtWidgets.QLabel(self.centralwidget)
+        self.labelAudio2.setGeometry(QtCore.QRect(820, 130, 171, 16))
+        self.labelAudio2.setObjectName("labelAudio2")
+        self.labelVideo3 = QtWidgets.QLabel(self.centralwidget)
+        self.labelVideo3.setGeometry(QtCore.QRect(820, 160, 171, 16))
+        self.labelVideo3.setObjectName("labelVideo3")
+        self.sliderCutoff = QtWidgets.QSlider(self.centralwidget)
+        self.sliderCutoff.setGeometry(QtCore.QRect(240, 340, 160, 22))
+        self.sliderCutoff.setOrientation(QtCore.Qt.Horizontal)
+        self.sliderCutoff.setObjectName("sliderCutoff")
+        self.sliderResolution = QtWidgets.QSlider(self.centralwidget)
+        self.sliderResolution.setGeometry(QtCore.QRect(240, 380, 160, 22))
+        self.sliderResolution.setOrientation(QtCore.Qt.Horizontal)
+        self.sliderResolution.setObjectName("sliderResolution")
+        self.sliderReduction = QtWidgets.QSlider(self.centralwidget)
+        self.sliderReduction.setGeometry(QtCore.QRect(240, 420, 160, 22))
+        self.sliderReduction.setOrientation(QtCore.Qt.Horizontal)
+        self.sliderReduction.setObjectName("sliderReduction")
+        self.labelCutoff = QtWidgets.QLabel(self.centralwidget)
+        self.labelCutoff.setGeometry(QtCore.QRect(140, 340, 47, 13))
+        self.labelCutoff.setObjectName("labelCutoff")
+        self.labelResolution = QtWidgets.QLabel(self.centralwidget)
+        self.labelResolution.setGeometry(QtCore.QRect(140, 380, 51, 16))
+        self.labelResolution.setObjectName("labelResolution")
+        self.labelReduction = QtWidgets.QLabel(self.centralwidget)
+        self.labelReduction.setGeometry(QtCore.QRect(140, 420, 91, 21))
+        self.labelReduction.setObjectName("labelReduction")
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(550, 470, 118, 23))
+        self.progressBar.setProperty("value", 24)
+        self.progressBar.setObjectName("progressBar")
+        self.labelCutoffUnits = QtWidgets.QLabel(self.centralwidget)
+        self.labelCutoffUnits.setGeometry(QtCore.QRect(430, 340, 47, 13))
+        self.labelCutoffUnits.setObjectName("labelCutoffUnits")
+        self.labelResolutionUnits = QtWidgets.QLabel(self.centralwidget)
+        self.labelResolutionUnits.setGeometry(QtCore.QRect(430, 380, 47, 13))
+        self.labelResolutionUnits.setObjectName("labelResolutionUnits")
+        self.labelReductionUnits = QtWidgets.QLabel(self.centralwidget)
+        self.labelReductionUnits.setGeometry(QtCore.QRect(430, 420, 47, 13))
+        self.labelReductionUnits.setObjectName("labelReductionUnits")
+        self.finalVideoView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.finalVideoView.setGeometry(QtCore.QRect(440, 520, 311, 211))
+        self.finalVideoView.setObjectName("finalVideoView")
+        self.buttonPlay = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonPlay.setGeometry(QtCore.QRect(490, 750, 75, 23))
+        self.buttonPlay.setObjectName("buttonPlay")
+        self.buttonSave = QtWidgets.QPushButton(self.centralwidget)
+        self.buttonSave.setGeometry(QtCore.QRect(630, 750, 75, 23))
+        self.buttonSave.setObjectName("buttonSave")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1034, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-        
-                
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Directory"))
+        self.labelVideo1.setText(_translate("MainWindow", "Video 1"))
+        self.labelAudio1.setText(_translate("MainWindow", "Audio 1"))
+        self.labelVideo2.setText(_translate("MainWindow", "Video 2"))
+        self.labelAudio2.setText(_translate("MainWindow", "Audio 2"))
+        self.labelVideo3.setText(_translate("MainWindow", "Video 3"))
+        self.labelCutoff.setText(_translate("MainWindow", "Cutoff"))
+        self.labelResolution.setText(_translate("MainWindow", "Resolution"))
+        self.labelReduction.setText(_translate("MainWindow", "Volume Reduction"))
+        self.labelCutoffUnits.setText(_translate("MainWindow", "dBFS"))
+        self.labelResolutionUnits.setText(_translate("MainWindow", "s"))
+        self.labelReductionUnits.setText(_translate("MainWindow", "dBFS"))
+        self.buttonPlay.setText(_translate("MainWindow", "Play"))
+        self.buttonSave.setText(_translate("MainWindow", "Save"))
+
